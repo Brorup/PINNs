0dc5d43c030bff1b6349319c56bf3d00e231c4c7
antj <antj@mmmi.sdu.dk>
2024-12-17 17:44:48 +0100

    Add pip install script

---
+++ PINN_framework/pip_install.sh
@@ -0,0 +1,10 @@
+python -m pip install --upgrade tqdm 
+python -m pip install --upgrade matplotlib 
+python -m pip install --upgrade numpy
+python -m pip install --upgrade "jax[cuda12]"
+# python -m pip install --upgrade jax
+python -m pip install --upgrade flax 
+python -m pip install --upgrade optax 
+python -m pip install --upgrade orbax-checkpoint 
+python -m pip install --upgrade torch --index-url https://download.pytorch.org/whl/cpu
+python -m pip install --upgrade scikit-learn
